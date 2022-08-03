484f8cde1a139eacd167f8c7c4734a141ca5ce32
Teresa <luvmargaritas@gmail.com>
2022-08-03 17:30:54 -0500

    add error handling

--- server.js
+++ server.js
@@ -13,26 +13,41 @@ const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3002;
-/**Routes we will use to access our end point
- * * .get() is an express method
- * it correlates to axios.get
- * the first parameter is a URL in quote
- * the second is a callback function
- */
+
 
 //our root of site pass callback(two, param)
 app.get('/', (request, response) => {
   response.send('hello from cyberland!!!!');
 });
 
-// localhost:3001/hello?name=bob
-// query: { name: 'bob' }, and the URL in the req
-// app.get('/hello' (request, response) => {
-//     console.log('request object: ', request.query.name);
-//     let location = request.query.location;
-//     let lat = request.query.lat;
-//     let lon = request.query.lon;
-// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Errors
+
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+});
+
+
+
+
+
+
+
+
 
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
