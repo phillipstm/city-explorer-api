6232569fcac747b6fc7e7fcce3d18f05e7b9a57c
Teresa <luvmargaritas@gmail.com>
2022-09-15 15:01:41 -0500

    add weather data to state

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict';
-
 console.log('!!!!dummmmmbyserver.js connected.');
+
+//*******Require
+//1.npm i express
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// let weather = require('./modules/weather.js');
+let weather = require('./modules/weather.js');
 const app = express();
 const PORT = process.env.PORT || 3002;
 
@@ -20,14 +22,13 @@ app.get('/weather', weatherHandler);
 
 function weatherHandler(request, response) {
   console.log(request.query);
-  // const { lat, lon } = request.query;
-  // weather(lat, lon)
-  //   .then(summaries => response.send(summaries))
-  //   .catch((error) => {
-  //     console.error(error);
-  //     response.status(200).send('Sorry. Something went wrong!');
-  //   });
-  response.status(200).send('not yet!');
+  const { lat, lon } = request.query;
+  weather(lat, lon)
+    .then(summaries => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(500).send('Sorry. Something went wrong!');
+    });
 }
 
 
